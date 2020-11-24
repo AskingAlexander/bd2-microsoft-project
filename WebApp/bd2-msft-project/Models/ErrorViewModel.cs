acb0854ed31ebe84af8528b00e6db7f88b9ad03f
Alexandru Petrescu <alexandru.petrescu@exghts.com>
2020-11-24 11:17:37 +0200

    Added Core Web App

---
+++ WebApp/bd2-msft-project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bd2_msft_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
