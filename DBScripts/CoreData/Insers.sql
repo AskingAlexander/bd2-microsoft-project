aaec9cd694ff63645fc53648f9c69b9f7ee5d27b
Alexandru Petrescu <alexandru.petrescu@exghts.com>
2020-11-24 11:10:11 +0200

    Added DB Structure

---
+++ DBScripts/CoreData/Insers.sql
@@ -0,0 +1,7 @@
+INSERT INTO [dbo].[StoreRole]
+           ([RoleName]
+           ,[RoleDescription])
+VALUES
+           ('Admin', 'Anything is possible when you are an Admin'), 
+           ('Editor', 'Basically a worker.'), 
+           ('User', 'Basically a payer.')
